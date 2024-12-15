fc6c8d8435d52f926b0f918f18fa2aa3c43aefa6
MShubar <pokeeeeepow@gmail.com>
2024-12-15 08:03:46 +0300

    Fixed after IA comments

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-console.log('Exercise 1: ')
+console.log("Exercise 1: ")
 console.log(``)
 const nums = [13, 87, 2, 89, 12, 4, 90, 63]
 const numss = nums.map((currentElement) => {
@@ -7,71 +7,69 @@ const numss = nums.map((currentElement) => {
 console.log(numss)
 
 console.log(`Exercise 2: `)
-const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies']
+const pizzaToppings = ["Pineapple", "Olives", "Anchovies"]
 const [firstTopping, secondTopping] = pizzaToppings
 console.log(firstTopping)
 console.log(secondTopping)
 
 console.log(`Exercise 3: `)
 const car = {
-  make: 'Audi',
-  model: 'q5'
+  make: "Audi",
+  model: "q5",
 }
 const { make, model } = car
 console.log(make)
 console.log(model)
 
 console.log(`Exercise 4: `)
-const pizzaTopping = ['Pineapple', 'Olives', 'Anchovies']
-const controversialPizzaToppings = pizzaToppings
+const controversialPizzaToppings = [...pizzaToppings]
+console.log(controversialPizzaToppings)
 
 console.log(controversialPizzaToppings)
 
 console.log(`Exercise 5: `)
-const myCar = car
-myCar.model = 'q7'
+const myCar = { ...car, model: "q7" }
 console.log(myCar)
+console.log(car)
 
 console.log(`Exercise 6: `)
-let propertyName = 'Tubli'
+let propertyName = "username"
 const userProfile = {
-  username: 'Mohsen',
-  age: '23',
-  email: 'mohsen.salman099@gmail.com'
+  username: "Mohsen",
+  age: "23",
+  email: "mohsen.salman099@gmail.com",
 }
 
-const selectedFruitCount = userProfile[propertyName]
-
-console.log(selectedFruitCount)
+console.log(userProfile[propertyName])
 
 console.log(`Exercise 7: `)
-import name, { age, job } from './exportingFile.js'
+import name, { age, job } from "./exportingFile.js"
 console.log(name, age, job)
 
 console.log(`Exercise 8: `)
-const addThreeNumbers = (noun, adjective) => {
-  return 'the ' + noun + ' is ' + adjective
+const describeAnimal = (noun, adjective) => {
+  return "The " + noun + " is " + adjective
 }
 
-addThreeNumbers('cat', 'white')
+console.log(describeAnimal("cat", "white"))
 
-console.log('Exercise 9: ')
-let pizza = 'tasty'
-console.log(pizza === 'tasty' ? 'yum' : 'yuck')
+console.log("Exercise 9: ")
+let pizza = "tasty"
+console.log(pizza === "tasty" ? "yum" : "yuck")
 
 console.log(`Exercise 10: `)
 const localLangConfig = null
-const LANG = localLangConfig || 'en'
-console.log('Language setting:', LANG)
+const LANG = localLangConfig || "en"
+console.log("Language setting:", LANG)
 
 const userSavedTheme = null
-const USER_THEME = userSavedTheme || 'light'
+const USER_THEME = userSavedTheme || "light"
 
-console.log('User theme setting:', USER_THEME)
+console.log("User theme setting:", USER_THEME)
 
 console.log(`Exercise 11:`)
 const adventurer = {
-  name: 'Alice'
+  name: "Alice",
 }
 
 let cat
